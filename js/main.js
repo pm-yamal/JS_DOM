eb89c165a5cac8cd28100f1e2d95e0a3eb197e9f
Marat <90244037+pm-yamal@users.noreply.github.com>
2024-03-27 21:57:57 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// 1. Найти элемент <body>
+const body = document.body;
+console.log(body);
+
+// 2. Создать элемент <header>
+const header = document.createElement('header');
+console.log(header);
+
+// 3. Вставить в <header> картинку
