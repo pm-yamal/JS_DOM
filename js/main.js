f9b06f4886c8f7c6c37c0f31af49784e838bbdee
Marat <90244037+pm-yamal@users.noreply.github.com>
2024-03-27 23:31:05 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
 // 1. Найти элемент <body>
-const body = document.body;
-console.log(body);
+const bodyElement = document.body;
+console.log(bodyElement);
+
+const lastChild = bodyElement.lastChild;
+console.log(lastChild);
 
 // 2. Создать элемент <header>
 const header = document.createElement('header');
 console.log(header);
 
 // 3. Вставить в <header> картинку
+const headerTop = document.createElement('header-top');
+console.log(headerTop);
+
+const container = document.createElement('container');
+console.log(container);
+
+const headerTopRow = document.createElement('header-top__row');
+console.log(headerTopRow);
+
+const headerTopLogo = document.createElement('header-top__logo');
+console.log(headerTopLogo);
+
+const headerTopNav = document.createElement('header-top__nav');
+console.log(headerTopNav);
+
+const headerTopNavContent = document.createElement('header-top__nav-content');
+console.log(headerTopNavContent);
+
+const headerTopNavContentUl = document.createElement('ul');
+console.log(headerTopNavContentUl);
+
+const headerTopNavContentLi = document.createElement('li');
+console.log(headerTopNavContentLi);
