2d5411a34643c68dc6955473331296062854afad
Marat <90244037+pm-yamal@users.noreply.github.com>
2024-03-28 23:34:18 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,35 +1,53 @@
 // 1. Найти элемент <body>
-const bodyElement = document.body;
-console.log(bodyElement);
-
-const lastChild = bodyElement.lastChild;
-console.log(lastChild);
+const body = document.body;
+console.log(body);
 
 // 2. Создать элемент <header>
-const header = document.createElement('header');
-console.log(header);
 
-// 3. Вставить в <header> картинку
-const headerTop = document.createElement('header-top');
+// Создание элемента div
+const headerTop = document.createElement('div');
 console.log(headerTop);
 
-const container = document.createElement('container');
-console.log(container);
+// Добавление к элементу div класса header-top
+headerTop.classList.add('header-top');
 
-const headerTopRow = document.createElement('header-top__row');
-console.log(headerTopRow);
+// Добавление элемента div внутрь элемента body
+body.appendChild(headerTop);
 
-const headerTopLogo = document.createElement('header-top__logo');
-console.log(headerTopLogo);
+// Создание container внутри header-top
+const container = document.createElement('div');
+container.classList.add('container');
+headerTop.appendChild(container);
 
-const headerTopNav = document.createElement('header-top__nav');
-console.log(headerTopNav);
+// Создание header-top__row внутри container 
+const headerTopRow = document.createElement('div');
+headerTopRow.classList.add('header-top__row');
+container.appendChild(headerTopRow);
 
-const headerTopNavContent = document.createElement('header-top__nav-content');
-console.log(headerTopNavContent);
+// Создание header-top__logo внутри header-top__row
+const headerTopLogo = document.createElement('div');
+headerTopLogo.classList.add('header-top__logo');
+headerTopRow.appendChild(headerTopLogo);
 
-const headerTopNavContentUl = document.createElement('ul');
-console.log(headerTopNavContentUl);
+// Создание header-top__nav внутри header-top__row
+const headerTopNav = document.createElement('div');
+headerTopNav.classList.add('header-top__nav');
+headerTopRow.appendChild(headerTopNav);
 
-const headerTopNavContentLi = document.createElement('li');
-console.log(headerTopNavContentLi);
+// 3. Вставить в <header> картинку
+
+// Создание логотипа внутри header-top__logo
+const img = document.createElement('img');
+img.src = './img/vector.png';
+headerTopLogo.appendChild(img);
+
+
+
+// const headerTopNavContent = document.createElement('header-top__nav-content');
+// console.log(headerTopNavContent);
+
+// const headerTopNavContentUl = document.createElement('ul');
+// console.log(headerTopNavContentUl);
+
+// const headerTopNavContentLi = document.createElement('li');
+// console.log(headerTopNavContentLi);
