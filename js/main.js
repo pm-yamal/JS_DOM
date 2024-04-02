ac2b77170a99f9297f35a1be4773eff6aedac260
Marat <90244037+pm-yamal@users.noreply.github.com>
2024-04-02 23:21:50 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,15 +7,11 @@ const header = document.createElement('header');
 header.classList.add('header');
 body.appendChild(header);
 
+// header-background
 header.style.backgroundColor = '#E5FF76E5';
 header.style.height = '900px';
 
-const backImg = document.createElement('img');
-backImg.src = './img/background-image.png';
-header.appendChild(backImg);
-backImg.style.width = '700px';
-
-
+// 3. header-top
 // Создание элемента div
 const headerTop = document.createElement('div');
 console.log(headerTop);
@@ -41,22 +37,19 @@ const headerTopLogo = document.createElement('div');
 headerTopLogo.classList.add('header-top__logo');
 headerTopRow.appendChild(headerTopLogo);
 
-// 3. Вставить в <header> картинку
-
+// Вставить в header-top логотип
 // Создание логотипа внутри header-top__logo
 const img = document.createElement('img');
 img.src = './img/vector.png';
 headerTopLogo.appendChild(img);
 
-// 4. Создать nav
-
+// 4. Создание навигации header-top__nav
 // Создание header-top__nav внутри header-top__row
 const headerTopNav = document.createElement('div');
 headerTopNav.classList.add('header-top__nav');
 headerTopRow.appendChild(headerTopNav);
 
-
-// 5. Вставить в nav список; 6. Вставить в список элементы с текстом
+// Вставить в nav список; Вставить в список элементы с текстом
 
 // Создание списка
 const ul = `
@@ -119,12 +112,67 @@ headerTopRow.style.display = 'flex';
 headerTopRow.style.justifyContent = 'space-between';
 
 
+// Создание header-title
+const headerTitle = document.createElement('div');
+headerTitle.classList.add('header-title');
+header.appendChild(headerTitle);
 
-// const headerTopNavContent = document.createElement('header-top__nav-content');
-// console.log(headerTopNavContent);
+// Создание header-title__name
+const headerTitleName = document.createElement('div');
+headerTitleName.classList.add('header-title__name');
+headerTitle.appendChild(headerTitleName);
 
-// const headerTopNavContentUl = document.createElement('ul');
-// console.log(headerTopNavContentUl);
+const h1 = document.createElement('h1');
+headerTitleName.appendChild(h1);
+h1.textContent = 'Finally, all your team’s work in one place';
 
-// const headerTopNavContentLi = document.createElement('li');
-// console.log(headerTopNavContentLi);
+// Создание header-title__text
+const headerTitleText = document.createElement('div');
+headerTitleText.classList.add('header-title__text');
+headerTitle.appendChild(headerTitleText);
+
+const p = document.createElement('p');
+headerTitleText.appendChild(p);
+p.textContent = 'Increase your team’s speed,collaboration, and alignment by giving everyone an overview of the most important work happening across your company.'
+
+// Создание header-title__buttons
+const headerTitleButtons = document.createElement('div');
+headerTitleButtons.classList.add('header-title__buttons');
+headerTitle.appendChild(headerTitleButtons);
+
+// button-left
+const buttonLeft = document.createElement('a');
+buttonLeft.classList.add('button');
+headerTitleButtons.appendChild(buttonLeft);
+buttonLeft.textContent = 'Get started for free';
+
+// button-right
+const buttonRight = document.createElement('a');
+buttonRight.classList.add('button');
+headerTitleButtons.appendChild(buttonRight);
+buttonRight.textContent = 'Watch video';
+
+const p1 = document.createElement('p');
+headerTitleButtons.append(p1);
+p1.textContent = 'No credit card required';
+
+// Создание header-banner
+const headerBanner = document.createElement('div');
+headerBanner.classList.add('header-banner');
+header.appendChild(headerBanner);
+
+const backImg = document.createElement('img');
+backImg.src = './img/background-image.png';
+headerBanner.appendChild(backImg);
+backImg.style.width = '420px';
+
+
+// Создание header-company
+const headerCompany = document.createElement('div');
+headerCompany.classList.add('header-company');
+header.appendChild(headerCompany);
+
+// Создание main
+const main = document.createElement('main');
+main.classList.add('main');
+body.appendChild(main);
